99d4bb4295646f7415cf01aefae8e51335b87dd5
Blasidelgado <blasidelgado@gmail.com>
2022-07-01 19:17:32 -0300

    Create function to evaluate and output the round winner

--- index.js
+++ index.js
@@ -11,27 +11,26 @@ WE WANT TO CREATE A SIMPLE "ROCK-PAPER-SCISSORS" FOR USERS TO PLAY AGAINST COMPU
 
 //1- CREATE A VARIABLE AND STORE USER INPUT IN IT.//
 let userChoice = prompt("Paper, Rock or Scissors?" , "");
+userChoice = userChoice.toUpperCase();
 //2- MAKE A HELPER FUNCTION TO AVOID INVALID INPUTS
 function inputChecker(userChoice) {
     if (userChoice === null || userChoice === "") {
-        alert ("No values found. Reload to try again")
+        console.log ("No values found. Reload to try again")
         return;
     }
-    userChoice = userChoice.toUpperCase();
-   if (userChoice === "PAPER" || userChoice === "ROCK" || userChoice === "SCISSOR") {
+   else if (userChoice === "PAPER" || userChoice === "ROCK" || userChoice === "SCISSOR") {
     console.log ("ready to play");
-    playerSelection(userChoice);
-    computerSelection(computerChoice);
+    oneRound (playerSelection, computerSelection);
     }
     else {
-        console.log ("no values found. Select between paper rock or scissor")
+        console.log ("Incorrect. Restart and Select between paper rock or scissor")
         return;
     }
 }
 inputChecker(userChoice);
 // 3- CREATE A FUNCTION FOR COMPUTER TO CREATE A RANDOM VALUE BETWEEN: ("PAPER", "ROCK", "SCIZOR")//
 function computerChoice () {
-    const computerOptions = ['paper','rock','scissors'];
+    const computerOptions = ['PAPER','ROCK','SCISSOR'];
     const choiceNumber = Math.floor(Math.random()*3);
     const computerPlay = computerOptions[choiceNumber];
     return computerPlay;
@@ -39,10 +38,41 @@ function computerChoice () {
 
 //4- CREATE TWO FUNCTIONS TO USE FOR COMPARE USER VS COMPUTER //
 function playerSelection (userChoice){
-    console.log (userChoice);
     return userChoice;
 }
 function computerSelection (computerChoice) {
-    console.log (computerChoice());
     return computerChoice();
 }
+function oneRound (playerSelection, computerSelection) {
+    if (playerSelection(userChoice) === computerSelection(computerChoice)) {
+        console.log ('draw')
+        return;
+    }
+    if ( playerSelection(userChoice) === "PAPER") {
+        if (computerSelection(computerChoice) === "ROCK") {
+            console.log ("You win! Paper beats Rock.")
+            return;
+        }
+        else {
+            console.log ("You lost! Scissor beats Paper")
+            return;
+        }
+    }
+    if (playerSelection(userChoice) === "ROCK") {
+        if (computerSelection(computerChoice) === "SCISSOR") {
+            console.log ("You win! Rock beats Scissor")
+            return;
+        }
+        else {
+            console.log ("You lose! Paper beats Rock.")
+        }
+    }
+    if (playerSelection(userChoice) === "SCISSOR") {
+        if (computerSelection(computerChoice) === "PAPER") {
+            console.log ("You win! Scissor beats Paper");
+            return;
+        }
+        else {
+            console.log ("You lose! Rock beats Scissor")
+        }
+    }}
